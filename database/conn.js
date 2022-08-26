79662b0a3a016a5056b1c496daef74601fc5970d
jvitormcs <vitormouracs@gmail.com>
2022-08-26 12:15:58 -0300

    mudança de conn

--- database/conn.js
+++ database/conn.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('u997776439_db_realixo_sit', 'u997776439_ragnermoura', '"Django77' ,{
+const sequelize = new Sequelize('u997776439_db_realixo_sit', 'u997776439_ragnermoura', 'Django77' ,{
     host: 'sql566.main-hosting.eu',
     dialect: 'mysql'
 })
