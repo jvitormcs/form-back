6d189c53426d845fb1c2d6bfdef1919f923e50bd
jvitormcs <vitormouracs@gmail.com>
2022-09-01 13:22:23 -0300

    make it better

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(express.json())
 
 
 app.use('/form', cadastroRoutes)
-app.use('invest', InvestController.Register)
 
 const PORT = process.env.PORT || 8081
 
--- routes.js
+++ routes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const CadastroController = require('./controllers/CadastroController');
-
+import InvestController from './controllers/InvestController'
 
 router.post('/valida-dados', CadastroController.Register )
+router.post('/investidores', InvestController.Register)
 
 module.exports = router
