7185bcf98dd28e1b196a806a57dadd94a8c67f85
jvitormcs <vitormouracs@gmail.com>
2022-11-02 16:16:18 -0300

    alteração

--- routes.js
+++ routes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const CadastroController = require('./controllers/CadastroController');
-const InvestController = require('./controllers/InvestController')
+const InvestController = require('./controllers/InvestController');
+const SiteController = require('./controllers/SiteController');
 
 router.post('/valida-dados', CadastroController.Register )
 router.post('/investidores', InvestController.Register)
+router.post('/site', SiteController.cad)
 
 module.exports = router
